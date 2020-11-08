2c1d95922a275983d56c94a864c1c994a8dc80c6
Mohammadreza Shahsavari <reza.sh.81@gmail.com>
2020-11-08 13:54:02 +0330

    990718

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
+const count = 4
+
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 4
+        }
+    }
+    isEven(val) {
+        return val % 2 === 0 ? "Even" : "Odd";
+    }
   render = () =>
       <h4 className="bg-primary text-white text-center p-2 m-1">
-        Component Element
+          Number of things: {this.isEven(this.state.count)}
       </h4>
 }
